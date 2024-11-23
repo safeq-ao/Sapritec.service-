7415645ee23359dcc6a1a464438da82f51e3ff10
elma-devweb <elmaeira@gmail.com>
2024-11-23 19:23:12 +0100

    feat:corrigindo erro para o deploy

--- src/utils/context/autentication.jsx
+++ src/utils/context/autentication.jsx
@@ -3,7 +3,7 @@ import { useState, createContext } from "react";
 export const CProvider = createContext([]);
 
 export default function Context({ children }) {
-    const [logged, setLogged] = useState(false);
+    const [logged, setLogged] = useState(false)
   return (
     <CProvider.Provider value={{ logged, setLogged   }}>
       {children}
